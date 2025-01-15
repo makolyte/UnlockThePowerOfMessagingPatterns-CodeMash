8815d88318a3266fe1be84a5baafa8d9fa1f0e11
Maclain Wiltzer <mwiltzer@gmail.com>
2025-01-15 10:20:08 -0500

    Use configured topic name

--- labs/MessagingLabs/lab1-producer/Program.cs
+++ labs/MessagingLabs/lab1-producer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Azure.Messaging.ServiceBus;
 
-var connectionString = "Endpoint=sb://127.0.0.1;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
-var topicName = "publish-subscribe";
+var connectionString = "Endpoint=sb://127.0.0.1:5672;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
+var topicName = "codemash.fundamentals.point-to-point"; //change this to something in Config.json
 
 //Note: 'await using' because it's IAsyncDisposable instead of IDisposable
 await using var client = new ServiceBusClient(connectionString);
